ac4449030bbd60ece7256155ca2f0b65599049e9
parmalatinter <parmalatinter@gmail.com>
2016-08-31 01:46:46 +0900

    no message

--- gulp/tasks/server/routes/index.js
+++ gulp/tasks/server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json('index', { title: 'Express' });
+  res.status(200).json({ title: 'Express' })
 });
 
 module.exports = router;
