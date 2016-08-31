76eddf59f1509411323dc0aa21a945821bf889ef
Yosuke Kobayashi <parmalatinter@gmail.com>
2016-08-31 16:51:32 +0900

    [test] add user model to index

--- gulp/tasks/server/routes/index.js
+++ gulp/tasks/server/routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var User       = require('../models/user');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', {
-      message: 'index page'
+    User.find(function(err, users) {
+        if (err)
+            res.send(err);
+        res.render('index', {
+          message: JSON.stringify(users)
+        });
     });
+
 });
 
 module.exports = router;
