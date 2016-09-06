31e81c70c84ecdaed1f2d2d53039e5372fd26c7a
parmalatinter <parmalatinter@gmail.com>
2016-09-07 01:03:34 +0900

    [add] user bottom sheet

--- public/js/controllers/base.js
+++ public/js/controllers/base.js
@@ -54,7 +54,7 @@ app.controller('AppCtrl', function($scope, $rootScope, $mdBottomSheet, $mdSidena
         $scope.showListBottomSheet = function($event) {
             $scope.alert = '';
             $mdBottomSheet.show({
-                template: '<md-bottom-sheet class="md-list md-has-header"> <md-subheader>Settings</md-subheader> <md-list> <md-item ng-repeat="item in items"><md-item-content md-ink-ripple flex class="inset"> <a flex aria-label="{{item.name}}" ng-click="listItemClick($index)"> <span class="md-inline-list-icon-label">{{ item.name }}</span> </a></md-item-content> </md-item> </md-list></md-bottom-sheet>',
+                templateUrl: '/templates/bottomSheets/user.html',
                 controller: 'ListBottomSheetCtrl',
                 targetEvent: $event
             }).then(function(clickedItem) {
--- public/js/services/loading.js
+++ public/js/services/loading.js
@@ -12,7 +12,7 @@ app.controller('LoadingCtrl', function($scope, Loading) {
 app.directive('loading', function(Loading) {
     return {
         replace: true,
-        template: '<md-backdrop ng-if="loading.isLoding" flex class="md-opaque" tabindex="-1" style="position: fixed;z-index:1" layout="row" layout-align="center center"><md-progress-circular ng-disabled="false" class="md-warn md-hue-3" md-diameter="{{loading.speed}}"></md-progress-circular></md-backdrop>',
+        template: '<md-backdrop ng-class="{ hide : !loading.isLoding,  fadeOut : !loading.isLoding, fadeIn : loading.isLoding}" flex class="md-opaque animated" tabindex="-1" style="position: fixed;z-index:1" layout="row" layout-align="center center"><md-progress-circular ng-disabled="false" class="md-warn md-hue-3" md-diameter="{{loading.speed}}"></md-progress-circular></md-backdrop>',
         controller: 'LoadingCtrl'
     };
 });
