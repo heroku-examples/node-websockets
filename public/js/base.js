d7838a550427aa9971734eab1d241df09e51c26b
parmalatinter <parmalatinter@gmail.com>
2016-10-21 00:05:29 +0900

    [fix] md-theme

--- public/js/base.js
+++ public/js/base.js
@@ -1,16 +1,39 @@
 var app = angular.module('StarterApp', [
-    'ui.router',
-    'ngMaterial',
-    'ngMdIcons',
-    'ngResource',
-    'ngMessages',
-    'ngStorage',
-    'pascalprecht.translate',
-    'ngCookies',
-    'firebase',
-    'images-resizer',
-    'ngImgCrop',
-    'angularLazyImg',
-    'angularRangeSlider',
-    'angular.filter'
-]);
+        'ui.router',
+        'ngMaterial',
+        'ngMdIcons',
+        'ngResource',
+        'ngMessages',
+        'ngStorage',
+        'pascalprecht.translate',
+        'ngCookies',
+        'firebase',
+        'images-resizer',
+        'ngImgCrop',
+        'angularLazyImg',
+        'angularRangeSlider',
+        'angular.filter'
+    ])
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('yellow')
+            .accentPalette('blue');
+
+        // Define a theme for the Login dialogs;
+        // @see <md-dialog md-theme="login">...</md-dialog>
+
+        $mdThemingProvider.theme('login')
+            .primaryPalette('brown')
+            .accentPalette('yellow');
+    }).config(['$translateProvider', function($translateProvider, $window) {
+        var findLanguage = function() {
+            return document.documentElement.lang;
+        };
+        $translateProvider.useStaticFilesLoader({
+            prefix: '/json/lang_',
+            suffix: '.json'
+        });
+        $translateProvider.preferredLanguage(findLanguage());
+        lang = findLanguage();
+        $translateProvider.useLocalStorage();
+    }]);
