5555a30ed3075b31c25af5be5c7166165495f1ad
parmalatinter <parmalatinter@gmail.com>
2016-10-24 23:07:39 +0900

    [add] user message area

--- public/js/services/login.js
+++ public/js/services/login.js
@@ -56,7 +56,7 @@ app
                 };
 
                 var user = firebase.auth().currentUser;
-                user.token().get().then(function(idToken) {
+                user.getToken().then(function(idToken) {
                     Token.find({ token: idToken }).$promise.then(function(_token) {
                         $sessionStorage.token = _token;
                         checkUserToRedirect();
