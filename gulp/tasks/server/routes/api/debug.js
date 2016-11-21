5aa56bb53a3a9eca36cb32f0984c97d8c861bbad
Yosuke Kobayashi <parmalatinter@gmail.com>
2016-11-21 17:05:45 +0900

    [bug] debug

--- gulp/tasks/server/routes/api/debug.js
+++ gulp/tasks/server/routes/api/debug.js
@@ -24,7 +24,7 @@ router.use(function (req, res, next) {
             } else if (identification) {
                 req.session.token.token = identification.token;
                 next();
-            } else{
+            } else {
                 res.status(resCodes.INTERNAL_SERVER_ERROR.code).json();
             }
         });
@@ -43,7 +43,7 @@ router.route('/debug/updateAdminToken')
         FireBaseSearvice.updateAdminToken(req).then(function (adminToken) {
             if (!adminToken) {
                 res.status(resCodes.INTERNAL_SERVER_ERROR.code).json();
-            }else{
+            } else {
                 res.status(resCodes.OK.code).json(adminToken);
             }
         }, function (err) {
@@ -52,13 +52,32 @@ router.route('/debug/updateAdminToken')
 
     });
 
+router.route('/debug/jack')
+    // セッションチャットの取得 (POST http://localhost:3000/api/chats)
+    .post(function (req, res) {
+        var Identification = require('../../models/identification');
+        Identification.findOne({
+            uid: req.body.token.uid
+        }, function(err, user) {
+            if (err) {
+                res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
+            } else if (identification) {
+                req.session.token = identification;
+                req.session.isJack = true;
+                req.session.isEntry = user.isEntry;
+            } else {
+                res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
+            }
+        });
+    });
+
 router.route('/debug')
     // 一つのデバッグの情報を取得 (GET http://localhost:8000/api/debugs/:debug_id)
-    .get(function(req, res) {
+    .get(function (req, res) {
         //debug_idが一致するデータを探す．
         Debug.find({
             uid: req.session.token.uid
-        }, function(err, debug) {
+        }, function (err, debug) {
             if (err) {
                 res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
             } else {
@@ -67,10 +86,10 @@ router.route('/debug')
         });
     })
     // デバッグの作成 (POST http://localhost:3000/api/debugs)
-    .post(function(req, res) {
+    .post(function (req, res) {
         Debug.findOne({
             uid: req.body.uid
-        }, function(err, debug) {
+        }, function (err, debug) {
             if (err) {
                 res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
             } else if (debug) {
@@ -82,14 +101,14 @@ router.route('/debug')
                 // デバッグの各カラムの情報を取得する
                 req.session.token = {};
                 _debug.uid = req.body.uid;
-                _debug.save(function(err) {
-                        if (err) {
-                            res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
-                        } else {
-                            res.status(resCodes.OK.code).json(_debug);
-                        }
-                    })
-                    .catch(function(err) {
+                _debug.save(function (err) {
+                    if (err) {
+                        res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
+                    } else {
+                        res.status(resCodes.OK.code).json(_debug);
+                    }
+                })
+                    .catch(function (err) {
                         res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
                     });
                 // });
@@ -97,57 +116,57 @@ router.route('/debug')
         });
     })
 
-// 一つのデバッグの情報を更新 (PUT http://localhost:8000/api/debugs/:debug_id)
-.put(function(req, res) {
-    Debug.findOne({
-        uid: req.body.uid
-    }, function(err, debug) {
-        if (err) {
-            res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
-        } else if (debug) {
-            // デバッグの各カラムの情報を更新する．
-            debug.delFlag = req.body.delFlag ? req.body.delFlag : false;
-            debug.save(function(err) {
-                if (err) {
-                    res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
-                } else {
-                    // ユーザーのデバッグ情報を更新する．
-                    User.findOneAndUpdate({ uid: req.body.uid }, { isDebug: !debug.delFlag }, function(err, user) {
-                        if (err) {
-                            res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
-                        } else {
-                            res.status(resCodes.OK.code).json(debug);
-                        }
-                    });
-                }
-            });
-        } else {
-            res.status(resCodes.NOT_FOUND.code).json(err);
-        }
-    });
-})
+    // 一つのデバッグの情報を更新 (PUT http://localhost:8000/api/debugs/:debug_id)
+    .put(function (req, res) {
+        Debug.findOne({
+            uid: req.body.uid
+        }, function (err, debug) {
+            if (err) {
+                res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
+            } else if (debug) {
+                // デバッグの各カラムの情報を更新する．
+                debug.delFlag = req.body.delFlag ? req.body.delFlag : false;
+                debug.save(function (err) {
+                    if (err) {
+                        res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
+                    } else {
+                        // ユーザーのデバッグ情報を更新する．
+                        User.findOneAndUpdate({ uid: req.body.uid }, { isDebug: !debug.delFlag }, function (err, user) {
+                            if (err) {
+                                res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
+                            } else {
+                                res.status(resCodes.OK.code).json(debug);
+                            }
+                        });
+                    }
+                });
+            } else {
+                res.status(resCodes.NOT_FOUND.code).json(err);
+            }
+        });
+    })
 
-// 一つのデバッグの情報を削除 (DELETE http://localhost:8000/api/debugs/:name)
-.delete(function(req, res) {
-    Debug.remove({
-        name: req.body.uid
-    }, function(err, debug) {
-        if (err) {
-            res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
-        } else {
-            res.status(resCodes.OK.code).json({
-                message: 'Successfully deleted!'
-            });
-        }
+    // 一つのデバッグの情報を削除 (DELETE http://localhost:8000/api/debugs/:name)
+    .delete(function (req, res) {
+        Debug.remove({
+            name: req.body.uid
+        }, function (err, debug) {
+            if (err) {
+                res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
+            } else {
+                res.status(resCodes.OK.code).json({
+                    message: 'Successfully deleted!'
+                });
+            }
+        });
     });
-});
 
 router.route('/debugs')
     // 全てのデバッグ一覧を取得 (GET http://localhost:8080/api/debugs)
-    .get(function(req, res) {
+    .get(function (req, res) {
         var page = req.query.page ? req.query.page : pageConfig.page;
         var limit = req.query.limit ? req.query.limit : pageConfig.limit;
-        Debug.paginate({}, { page: page, limit: limit }, function(err, result) {
+        Debug.paginate({}, { page: page, limit: limit }, function (err, result) {
             if (err) {
                 res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
             } else {
@@ -156,18 +175,18 @@ router.route('/debugs')
         });
     })
 
-// 条件指定で対象デバッグ一覧を取得 (GET http://localhost:8080/api/debugs/find)
-.post(function(req, res) {
-    var page = req.query.page ? req.query.page : pageConfig.page;
-    var limit = req.query.limit ? req.query.limit : pageConfig.limit;
-    Debug.paginate(req.body, { page: page, limit: limit }, function(err, debugs) {
-        if (err) {
-            res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
-        } else {
-            res.status(resCodes.OK.code).json(debugs);
-        }
+    // 条件指定で対象デバッグ一覧を取得 (GET http://localhost:8080/api/debugs/find)
+    .post(function (req, res) {
+        var page = req.query.page ? req.query.page : pageConfig.page;
+        var limit = req.query.limit ? req.query.limit : pageConfig.limit;
+        Debug.paginate(req.body, { page: page, limit: limit }, function (err, debugs) {
+            if (err) {
+                res.status(resCodes.INTERNAL_SERVER_ERROR.code).json(err);
+            } else {
+                res.status(resCodes.OK.code).json(debugs);
+            }
+        });
     });
-});
 
 // ルーティング登録
 module.exports = router;
