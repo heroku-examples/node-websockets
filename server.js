64fb6e1dfcbb5a3e2ba97a2d63fc6665ea8bb689
Jon Duke <jduke99@gmail.com>
2023-09-25 22:36:34 -0400

    to other server

--- server.js
+++ server.js
@@ -12,13 +12,42 @@ const server = express()
 
 const wss = new Server({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
+// wss.on('connection', (ws) => {
+//   console.log('Client connected');
+//   ws.on('close', () => console.log('Client disconnected'));
+// });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
+
+let gameState = []
+
+console.log(`running on ws://127.0.0.1:${port}`)
+
+wss.on('connection', (ws) => {
+
+  ws.on('message', message => {
+    let data = JSON.parse(message)
+    
+    if(gameState.filter(el => el.id == data.id).length > 0) {
+    	gameState = gameState.map(el => {
+    		if(el.id == data.id) return data
+    		else return el
+    	})
+    } else {
+    	console.log(`new player, id: ${data.id}`)
+	  	gameState.push(data)
+    }
+
+    ws.send(JSON.stringify(gameState))
+  })
+
+  ws.on('close', (code, reason) => {
+  	gameState = gameState.filter(el => el.id != parseInt(reason))
+  	console.log(`player ${reason} disconnected`)
+  })
+ 
+})
