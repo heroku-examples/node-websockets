423b7d01994f57452a094bec4d718fcc7547f70d
Jon Duke <jduke99@gmail.com>
2023-09-26 13:59:32 -0400

    test

--- server.js
+++ server.js
@@ -1,24 +1,66 @@
-'use strict';
+const WebSocket = require('ws')
+const gdCom = require('@gd-com/utils')
 
-const express = require('express');
-const { Server } = require('ws');
+const wss = new WebSocket.Server({ port: 8080 })
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+wss.on('connection', ws => {
+  console.log('connected')
+  ws.on('message', (message) => {
+    let recieveBuff = Buffer.from(message)
+    let recieve = gdCom.getVar(recieveBuff)
+    console.log(recieve.value)
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    let buffer = gdCom.putVar(Math.random())
+    ws.send(buffer)
+  })
+})
 
-const wss = new Server({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
+// 'use strict';
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+// const express = require('express');
+// const { Server } = require('ws');
+
+// const PORT = process.env.PORT || 3000;
+// const INDEX = '/index.html';
+
+// let gameState = []
+
+// const server = express()
+//   // .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// const wss = new Server({ server });
+
+// wss.on('connection', (ws) => {
+//   console.log('Client connected');
+//   ws.on('close', () => console.log('Client disconnected'));
+//   ws.on('message', message => {
+//     let data = JSON.parse(message)
+    
+//     if(gameState.filter(el => el.id == data.id).length > 0) {
+//     	gameState = gameState.map(el => {
+//     		if(el.id == data.id) return data
+//     		else return el
+//     	})
+//     } else {
+//     	console.log(`new player, id: ${data.id}`)
+// 	  	gameState.push(data)
+//     }
+
+//     ws.send(JSON.stringify(gameState))
+//   })
+//   ws.on('close', (code, reason) => {
+//   	gameState = gameState.filter(el => el.id != parseInt(reason))
+//   	console.log(`player ${reason} disconnected`)
+//   })
+
+// });
+
+
+
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
